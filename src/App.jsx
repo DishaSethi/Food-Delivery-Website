0e66fc3ea1885fbad81f87f1d81d956a687a2285
Disha <sethidisha123@gmail.com>
2024-12-28 00:04:39 +0530

    login , signup popup made

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar.jsx';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home/Home.jsx'; // Adjust path if necessary
@@ -7,12 +7,17 @@ import PlaceOrder from './components/pages/PlaceOrder/PlaceOrder.jsx';
 import ExploreMenu from './components/ExploreMenu/ExploreMenu.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import AppDownload from './components/AppDownload/AppDownload.jsx';
+import LoginPopup from './components/LoginPopup/LoginPopup.jsx';
 
 const App = () => {
+
+  const [showLogin, setShowLogin]=useState(false);
+
   return (
     <>
+    {showLogin? <LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
-      <Navbar />
+      <Navbar setShowLogin={setShowLogin} />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/cart' element={<Cart />} />
