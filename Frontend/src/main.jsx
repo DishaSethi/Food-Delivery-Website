dd26772a8618e49036b2350fcdf9400736879718
Disha <sethidisha123@gmail.com>
2025-03-20 15:39:48 +0530

    navbar changes for dark mode

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import StoreContextProvider from './Context/StoreContext.jsx';
+import ThemeProvider from './Context/ThemeContext.jsx'; // Import ThemeProvider
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <StoreContextProvider>
-      <App />
-      </StoreContextProvider>
+      <ThemeProvider> {/* Wrap everything inside ThemeProvider */}
+        <StoreContextProvider>
+          <App />
+        </StoreContextProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );
